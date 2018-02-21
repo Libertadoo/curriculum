44dbc2cc22f6578f5a1297b24ec8990a869c8c9f
elitzca <elitzca0raycheva@gmail.com>
2018-02-21 12:50:38 +0100

    Home page + slideshow /html, css, js - external in the old documents - code can be used for slideshows multiple times.

--- script.js
+++ script.js
@@ -9,9 +9,31 @@ function myFunction() {
     }
 }
 
+
+
+
+// function for slideshow on Home page, code can be used for all
+var myIndex = 0;
+            carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
+
 fetch("json.json").then(result => result.json()).then(data => show(data));
 
 function show(skills){
 	//coreAreas.forEach(coreAreas=>console.log("smth"));
 	console.log(coreAreas);
 }
+
+
